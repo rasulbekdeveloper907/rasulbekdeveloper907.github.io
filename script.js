77b888abc32b5ea2a9a7db0b2d406544d935a7f6
Rasulbek <rassiazzi1892@gmail.com>
2026-04-30 21:08:36 +0500

    csdsdsdsdscc

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-// simple animation (optional)
-console.log("Portfolio loaded 🚀");
+console.log("Rasulbek Portfolio Loaded 🚀");
+
+document.querySelectorAll('a[href^="#"]').forEach(a => {
+  a.addEventListener("click", e => {
+    e.preventDefault();
+    document.querySelector(a.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
