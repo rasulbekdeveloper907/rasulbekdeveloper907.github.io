ea7d69f67f136daa43c5b98ae84a725cdd4f4618
Rasulbek <rassiazzi1892@gmail.com>
2026-04-30 21:13:19 +0500

    csdsd

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
-console.log("Rasulbek Portfolio Loaded 🚀");
+const text = [
+  "AI/ML Engineer 🤖",
+  "Data Scientist 📊",
+  "Deep Learning Developer 🧠"
+];
 
-document.querySelectorAll('a[href^="#"]').forEach(a => {
-  a.addEventListener("click", e => {
-    e.preventDefault();
-    document.querySelector(a.getAttribute("href")).scrollIntoView({
-      behavior: "smooth"
-    });
-  });
-});
+let i = 0;
+let j = 0;
+let currentText = "";
+let isDeleting = false;
+
+function type() {
+  currentText = text[i];
+
+  if (!isDeleting) {
+    document.querySelector(".typing").textContent =
+      currentText.substring(0, j++);
+  } else {
+    document.querySelector(".typing").textContent =
+      currentText.substring(0, j--);
+  }
+
+  if (j === currentText.length + 1) {
+    isDeleting = true;
+    setTimeout(type, 1000);
+    return;
+  }
+
+  if (j === 0) {
+    isDeleting = false;
+    i = (i + 1) % text.length;
+  }
+
+  setTimeout(type, isDeleting ? 50 : 100);
+}
+
+type();
